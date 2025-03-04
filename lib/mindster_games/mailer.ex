03c05e10c7f3ca3f0f786e8dc6d1aa326fb807b5
Rolf Malthe Andersen <rolf.m.andersen@twoday.com>
2025-03-04 17:32:40 +0100

    add phoenix initial

---
+++ lib/mindster_games/mailer.ex
@@ -0,0 +1,3 @@
+defmodule MindsterGames.Mailer do
+  use Swoosh.Mailer, otp_app: :mindster_games
+end
---
+++ lib/mindster_games_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule MindsterGamesWeb.PageController do
+  use MindsterGamesWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
