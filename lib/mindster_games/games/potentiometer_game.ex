0a0cfba4fa8834b8fb62b0480e9d21f6ea94d068
Jonas Nielsen <jonas.nielsen2@twoday.com>
2025-03-04 19:52:36 +0100

    AI made it into state_machine

--- lib/mindster_games/games/potentiometer_game.ex
+++ lib/mindster_games/games/potentiometer_game.ex
@@ -1,5 +1,5 @@
 defmodule MindsterGames.Games.PotentiometerGame do
-  use MindsterGames.Games.StateMachine
+  use StateMachine
 
   defstruct players: [],
             teams: [],
@@ -8,51 +8,54 @@ defmodule MindsterGames.Games.PotentiometerGame do
             current_hinter: nil,
             current_goal: nil,
             current_scale: nil,
-            current_guess: nil
+            current_guess: nil,
+            state: :awaiting_players
 
-  defmachine do
+  defmachine field: :state do
     state :awaiting_players
-    state :starting_game
-    state :hinter_picking
+    state :starting_game, after_enter: &MindsterGames.Games.PotentiometerGame.setup_game/1
+    state :hinter_picking, after_enter: &MindsterGames.Games.PotentiometerGame.setup_hinter_picking/1
     state :guesser_picking
-    state :reveal_result
+    state :reveal_result, after_enter: &MindsterGames.Games.PotentiometerGame.calculate_points/1
     state :game_finished
 
-    event :player_joined, from: :awaiting_players, to: :starting_game, if: player_count_valid?/2
-    event :hinter_ready, from: :starting_game, to: :hinter_picking
-    event :guesser_submits, from: :hinter_picking, to: :guesser_picking
-    event :guesser_submits, from: :guesser_picking, to: :reveal_result
+    event :player_joined do
+      transition from: :awaiting_players, to: :starting_game,
+      if: &MindsterGames.Games.PotentiometerGame.player_count_valid?/1
+    end
+
+    event :hinter_ready do
+      transition from: :starting_game, to: :hinter_picking
+    end
+
+    event :guesser_submits do
+      transition from: :hinter_picking, to: :guesser_picking
+      transition from: :guesser_picking, to: :reveal_result
+    end
   end
 
-
-
-  @impl MindsterGames.Games.StateMachine
   def mutate(state, :player_joined, data) do
     %{state | players: state.players ++ [data.player]}
   end
 
-  @impl MindsterGames.Games.StateMachine
   def mutate(state, :guesser_submits, data) do
     %{state | current_guess: data.guess}
   end
 
-
-
-  @impl MindsterGames.Games.StateMachine
-  def enter(state, :starting_game) do
-    %{
+  def setup_game(state) do
+    updated_state = %{
       state
       | round: 1,
-      teams: [
-        %{points: 0, players: [state.players[0], state.players[1]]},
-        %{points: 0, players: [state.players[2], state.players[3]]}
-      ],
-      current_hinter: state.players[0],
+        teams: [
+          %{points: 0, players: [state.players[0], state.players[1]]},
+          %{points: 0, players: [state.players[2], state.players[3]]}
+        ],
+        current_hinter: state.players[0]
     }
+    {:ok, updated_state}
   end
 
-  @impl MindsterGames.Games.StateMachine
-  def enter(state, :hinter_picking) do
+  def setup_hinter_picking(state) do
     random_goal = Enum.random(1..100)
 
     # full list in their translation sheet
@@ -65,11 +68,11 @@ defmodule MindsterGames.Games.PotentiometerGame do
       {"Meal", "Snack"}
     ] |> Enum.random()
 
-    %{state | current_goal: random_goal, current_scale: random_scale}
+    updated_state = %{state | current_goal: random_goal, current_scale: random_scale}
+    {:ok, updated_state}
   end
 
-  @impl MindsterGames.Games.StateMachine
-  def enter(state, :reveal_result) do
+  def calculate_points(state) do
     points = case abs(state.current_goal - state.current_guess) do
       diff when diff <= 10 -> 4
       diff when diff <= 20 -> 3
@@ -78,13 +81,11 @@ defmodule MindsterGames.Games.PotentiometerGame do
       _ -> 0
     end
 
-    %{state | current_team: state.current_team + points}
+    updated_state = %{state | current_team: state.current_team + points}
+    {:ok, updated_state}
   end
 
-
-
-  defp player_count_valid?(state) do
+  def player_count_valid?(state) do
     length(state.players) == 4
   end
-
 end
