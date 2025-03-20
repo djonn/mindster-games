856e5550c0dfa1b147fd71914d55997799dbd02f
Jonas Nielsen <djonnneller@gmail.com>
2025-03-20 18:38:37 +0100

    Add test for hinter being ready to pick

--- test/games/potentiometer_game_test.exs
+++ test/games/potentiometer_game_test.exs
@@ -1,6 +1,7 @@
 defmodule MindsterGames.Games.PotentiometerGameTest do
   use MindsterGamesWeb.DataCase
-  alias MindsterGames.Games.GameGenServer
+
+  alias MindsterGames.Games.{GameGenServer, PotentiometerGame}
 
   test "When new game starts then game is awaiting players" do
     {:ok, game_pid} = GameGenServer.start_link()
@@ -24,7 +25,7 @@ defmodule MindsterGames.Games.PotentiometerGameTest do
     assert state.state == :awaiting_players
   end
 
-  test "4 players join and the game starts" do
+  test "When player number 4 joins then the game starts" do
     {:ok, game_pid} = GameGenServer.start_link()
 
     player1 = "player1"
@@ -40,5 +41,23 @@ defmodule MindsterGames.Games.PotentiometerGameTest do
     state = GameGenServer.info(game_pid)
     assert state.players == [player1, player2, player3, player4]
     assert state.state == :starting_game
+  end
+
+  test "When starting game and hinter is ready then hinter can start picking" do
+    existing_state = %PotentiometerGame{
+      players: ["player1", "player2", "player3", "player4"],
+      state: :starting_game
+    }
+
+    {:ok, game_pid} = GameGenServer.start_link(existing_state)
+
+    GameGenServer.trigger(game_pid, :hinter_ready)
+
+    state = GameGenServer.info(game_pid)
+    assert state.state == :hinter_picking
+    assert is_number(state.current_goal)
+    assert {left, right} = state.current_scale
+    assert is_binary(left)
+    assert is_binary(right)
   end
 end
