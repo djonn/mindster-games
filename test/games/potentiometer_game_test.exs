7ece3a89f40f44e78c9a9dc50c74dd8e44723855
Jonas Nielsen <djonnneller@gmail.com>
2025-03-20 18:07:42 +0100

    Add tests for awaiting players

--- test/games/potentiometer_game_test.exs
+++ test/games/potentiometer_game_test.exs
@@ -2,6 +2,28 @@ defmodule MindsterGames.Games.PotentiometerGameTest do
   use MindsterGamesWeb.DataCase
   alias MindsterGames.Games.GameGenServer
 
+  test "When new game starts then game is awaiting players" do
+    {:ok, game_pid} = GameGenServer.start_link(nil)
+
+    state = GameGenServer.info(game_pid)
+    assert state.players == []
+    assert state.state == :awaiting_players
+  end
+
+  test "When fewer than 4 players join then game is still awaiting players" do
+    {:ok, game_pid} = GameGenServer.start_link(nil)
+
+    player1 = "player1"
+    player2 = "player2"
+
+    GameGenServer.trigger(game_pid, :player_joined, %{player: player1})
+    GameGenServer.trigger(game_pid, :player_joined, %{player: player2})
+
+    state = GameGenServer.info(game_pid)
+    assert state.players == [player1, player2]
+    assert state.state == :awaiting_players
+  end
+
   test "4 players join and the game starts" do
     {:ok, game_pid} = GameGenServer.start_link(nil)
 
@@ -19,5 +41,4 @@ defmodule MindsterGames.Games.PotentiometerGameTest do
     assert state.players == [player1, player2, player3, player4]
     assert state.state == :starting_game
   end
-
 end
