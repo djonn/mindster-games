21ef9d5aabb601cbb6dffee202606b7ffe7dbe6e
Jonas Nielsen <djonnneller@gmail.com>
2025-03-31 19:29:04 +0200

    Start implementing player page

--- lib/mindster_games_web/live/room/index.ex
+++ lib/mindster_games_web/live/room/index.ex
@@ -5,7 +5,46 @@ defmodule MindsterGamesWeb.Live.Room.Index do
   @impl true
   def render(assigns) do
     ~H"""
-    This is a room for {@room_id}
+    <div class="flex flex-col gap-12 w-full justify-center pb-24">
+      <.waiting_for_players :if={@game_state == :waiting_for_players} />
+      <.submit_number_potentiometer :if={@game_state == :submit_number_potentiometer} />
+      <.circles />
+    </div>
+    """
+  end
+
+  defp waiting_for_players(assigns) do
+    ~H"""
+    <div class="w-full items-center flex flex-col content-center gap-1.5 text-slate-900">
+      <h2 class="text-5xl">Waiting for other players</h2>
+    </div>
+    """
+  end
+
+  defp submit_number_potentiometer(assigns) do
+    # https://codepen.io/josetxu/pen/oNQxxyZ
+    ~H"""
+    <div class="w-full items-center flex flex-col content-center gap-1.5 text-slate-900">
+      <h2 class="text-5xl">Submit Number Potentiometer</h2>
+      <input type="range" />
+    </div>
+    """
+  end
+
+  defp circles(assigns) do
+    ~H"""
+    <ul class="circles -z-10">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
     """
   end
 
@@ -16,6 +55,8 @@ defmodule MindsterGamesWeb.Live.Room.Index do
     socket
     |> maybe_join_game(game_pid)
     |> assign(room_id: room_id)
+    |> assign(page_title: "#{room_id}")
+    |> assign(game_state: :submit_number_potentiometer)
     |> ok()
   end
 
