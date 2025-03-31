16c7a5c5b34b956b09ff6e1a6bf4c69f74ffb3c9
Jonas Nielsen <djonnneller@gmail.com>
2025-03-31 18:33:34 +0200

    starting writing tests + fixing stuff

---
+++ lib/mindster_games/games/game_gen_server.ex
@@ -0,0 +1,33 @@
+defmodule MindsterGames.Games.GameGenServer do
+  use GenServer
+
+  alias MindsterGames.Games.PotentiometerGame
+
+  def start_link(_initial_state, opts \\ []) do
+    initial_state = %PotentiometerGame{}
+    GenServer.start_link(__MODULE__, initial_state, opts)
+  end
+
+  def init(initial_state) do
+    {:ok, initial_state}
+  end
+
+  # ---- Client API -------
+  def trigger(pid, event, payload \\ nil) do
+    GenServer.call(pid, {:trigger, event, payload})
+  end
+
+  def info(pid) do
+    GenServer.call(pid, :info)
+  end
+
+  # ---- Server Callbacks -------
+  def handle_call({:trigger, event, payload}, _from, state) do
+    {:ok, new_state} = PotentiometerGame.trigger(state, event, payload)
+    {:reply, new_state, new_state}
+  end
+
+  def handle_call(:info, _from, state) do
+    {:reply, state, state}
+  end
+end
